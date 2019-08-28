f37dd99c1d3cc0c47ab0da361c10178738166ecf
Thomas Britz <britzthomas@gmail.com>
2019-08-28 12:01:26 +0100

    d

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :search]
 
   def home
   end
+
+  def search
+  end
 end
+
+
