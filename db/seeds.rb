910824dbe7959b93c075e2212b471c3957722206
Rokas Vaitkevicius <rokas.vaitkevicius1@gmail.com>
2019-08-28 16:28:05 +0100

    sm upd

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 user_1 = User.create!(
-  title: "Mr", first_name: "Bobby", last_name: "Mob", email: "bobberly@repay.com", password: "111111", phone_number: 555888, address: "Westminster, London SW1A 1AA", postcode: "ig3b14", city: "London", country: "United Kingdom", sort_code: "555999", account_number: "22233344")
+  title: "Mr", first_name: "Bobby", last_name: "Mob", email: "blobber@repay.com", password: "111111", phone_number: "asdqwe", address: "Westminster, London SW1A 1AA", postcode: "ig3b14", city: "London", country: "United Kingdom", sort_code: "555999", account_number: "22233344")
 
 ticket_9 = Ticket.create!(
   ticket_number: '55221123',
