de7ef44563e352e21b24a50ec0140a0d28c80c38
Rokas Vaitkevicius <rokas.vaitkevicius1@gmail.com>
2019-08-28 15:59:54 +0100

    seed test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ticket_9 = Ticket.create!(
+  ticket_number: '55221123',
+  price: 64.23,
+  valid_from: 20191111,
+  expiry_date: 20191112,
+  ticket_type: "season",
+  user: user_1)
+  url9 = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/British_rail_ticket_Wellington_Shrewsbury.jpg/250px-British_rail_ticket_Wellington_Shrewsbury.jpg"
+  venue_9.remote_ticket_photo_url = url9
+  venue_9.save!
