e97dedcd1e1a49b160dfece3d8a7fe5b39fbeb5c
Rokas Vaitkevicius <rokas.vaitkevicius1@gmail.com>
2019-08-29 11:17:52 +0100

    Added photo card controller with a new form

---
+++ app/controllers/photo_cards_controller.rb
@@ -0,0 +1,22 @@
+class PhotoCardsController < ApplicationController
+  def new
+    @photo_card = PhotoCard.new
+  end
+
+  def create
+    @photo_card = PhotoCard.new(photocard_params)
+    @user = current_user
+    @photo_card.user = @user
+    if @photo_card.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def photocard_params
+    params.require(:photo_card).permit(:photo, :card_number)
+  end
+end
