ef86426fc8b62b843412a903c4ed9172ee9ef132
Thomas Britz <britzthomas@gmail.com>
2019-08-30 15:56:22 +0100

    done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,25 @@ import 'flatpickr/dist/flatpickr.css'
 flatpickr("#ticket_expiry_date", { defaultDate: "today", altInput: true, altFormat: "F j, Y", dateFormat: "Y-m-d", });
 previewImageOnFileSelect()
 initSelect2();
+
+const ticket = document.querySelector('#claim_ticket_id')
+
+if (ticket) {
+  document.querySelector('#claim-ticket-select').addEventListener('change', (e) => {
+    ticket.value = e.currentTarget.value
+  })
+}
+
+const terms = document.querySelector('#terms')
+const submission = document.querySelector('#submission')
+
+if (terms) {
+  document.querySelector('#terms').addEventListener('change', (e) => {
+    if (submission.disabled) {
+      submission.disabled = false
+    } else {
+      submission.disabled = true
+    }
+  })
+}
+
