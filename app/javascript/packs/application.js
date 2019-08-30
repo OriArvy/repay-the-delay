e42b9ff31ac7b7cd9be968c4866699057b6d1d56
matt annetts <mot182@hotmail.com>
2019-08-30 15:06:52 +0100

    sweetalert added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,14 @@ import { initSelect2 } from '../plugins/init_select2';
 import flatpickr from "flatpickr";
 import 'flatpickr/dist/flatpickr.css'
 
+import { initSweetalert } from '../plugins/init_sweetalert';
+
 flatpickr("#ticket_expiry_date", { defaultDate: "today", altInput: true, altFormat: "F j, Y", dateFormat: "Y-m-d", });
 previewImageOnFileSelect()
 initSelect2();
+
+initSweetalert('#sweet-alert-demo', {
+  title: "Claim Submitted",
+  text: "We will keep you updated via email",
+  icon: "success"
+});
