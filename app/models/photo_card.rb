150f6cfea1d0d3e97274462307bb172b5d95e886
Rokas Vaitkevicius <rokas.vaitkevicius1@gmail.com>
2019-08-30 12:42:38 +0100

    Added photocard-fixes

--- app/models/photo_card.rb
+++ app/models/photo_card.rb
@@ -1,6 +1,6 @@
 class PhotoCard < ApplicationRecord
   belongs_to :user
   validates :photo, :card_number, presence: true
-  validates :card_number, uniqueness: true
+  validates :card_number, uniqueness: true, length: { minimum: 5 }
   mount_uploader :photo, PhotoUploader
 end
