7f33fcd823250a616bc1e77b70c1cd2f64762ae0
matt annetts <mot182@hotmail.com>
2019-09-02 10:46:14 +0100

    sweet alert sorting

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_30_110446) do
-
+ActiveRecord::Schema.define(version: 2019_08_30_075928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +34,6 @@ ActiveRecord::Schema.define(version: 2019_08_30_110446) do
     t.datetime "updated_at", null: false
     t.string "departure_date"
     t.string "departure_time"
-    t.string "status"
     t.index ["ticket_id"], name: "index_claims_on_ticket_id"
     t.index ["user_id"], name: "index_claims_on_user_id"
   end
